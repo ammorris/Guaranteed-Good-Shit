65f9b98d4ddf5af4f66c50e4231a7092c83dac26
ammorris <37010476+ammorris@users.noreply.github.com>
2018-03-02 22:37:56 -0800

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,34 @@
+var snes = document.getElementById("snes");
+var genesis = document.getElementById("genesis");
+var playstation = document.getElementById("playstation");
+var n64 = document.getElementById("n64");
+var gamecube = document.getElementById("gamecube");
+var gameboy = document.getElementById("gameboy");
+var ds = document.getElementById("ds");
+
+snes.onclick = function() {
+	alert("it works");
+}
+genesis.onclick = function() {
+	alert("it works");
+}
+
+playstation.onclick = function() {
+	alert("it works");
+}
+
+n64.onclick = function() {
+	alert("it works");
+}
+
+gamecube.onclick = function() {
+	alert("it works");
+}
+
+gameboy.onclick = function() {
+	alert("it works");
+}
+
+ds.onclick = function() {
+	alert("it works");
+}
