48426f2ad44535489d4db5d2de2dca92f18a9d14
ammorris <37010476+ammorris@users.noreply.github.com>
2018-03-04 19:33:42 -0800

    +Clock & +Nav titles
    Added clock and titles to navigation buttons.

--- js/script.js
+++ js/script.js
@@ -5,6 +5,31 @@ var n64 = document.getElementById("n64");
 var gamecube = document.getElementById("gamecube");
 var gameboy = document.getElementById("gameboy");
 var ds = document.getElementById("ds");
+const clock = document.getElementById("clock");
+
+function blink() {
+    var colon = document.getElementById("colon");
+    colon.style.visibility = colon.style.visibility == "hidden" ? "visible" : "hidden";
+}
+
+document.body.onload = function() {
+    updateClock();
+    setInterval("updateClock()", 1000);
+    setInterval("blink()", 500);
+}
+
+function updateClock() {
+    var today = new Date();
+    var hours = today.getHours() % 12;
+    if (hours == 0) { hours = 12; }
+    var minutes = today.getMinutes();
+    var timeOfDay = (today.getHours()) < 12 ? "午前" : "午後";
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+    var time = hours + "<span id='colon'>:</span>" + minutes + timeOfDay;
+    document.getElementById('clock').innerHTML = time;
+}
 
 snes.onclick = function() {
 	alert("it works");
